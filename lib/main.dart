8955169a8a4e1c5f24f1ec479da496ad4e3cca5a
Yorman Valencia <yormanvale@outlook.com>
2023-04-15 13:12:24 -0500

    Vista Estimados de Producción

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:agroxpert/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:agroxpert/screens/EstimarProduccion/estimarProducciónView.dart';
+import 'package:agroxpert/routes/routes.dart';
+import 'package:agroxpert/screens/estimarProduccionView.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +17,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      // Aquí agregamos el generador de rutas
+      onGenerateRoute: Routes.generateRoute,
+      // La pantalla de inicio sigue siendo MyHomePage
       home: const MyHomePage(title: 'AgroXdemo Demo Page'),
     );
   }
@@ -32,14 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _isHovering = false;
-
-  void _toggleHover(bool isHovering) {
-    setState(() {
-      _isHovering = isHovering;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,38 +45,20 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'The person who has make this change was Samuel',
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/estimates_production');
+              },
+              child: const Text('Estimados de Producción'),
             ),
-
-            Image.network(
-              'https://picsum.photos/400/400',
-              width: 300,
-              height: 300,
-            ),
-            const Text(
-              'Random Image from the internet',
-            ),
-            MouseRegion(
-              onEnter: (_) => _toggleHover(true),
-              onExit: (_) => _toggleHover(false),
-              child: Text(
-                'Hola Mundo, from Yorman with hover :)',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: _isHovering ? Colors.red : Colors.black,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20), // agregar un espacio en blanco
-            const Text(
-              'Hello World, from Camila', // agregar el nuevo widget de texto
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
+                const SizedBox(
+                      height: 20,
+                    ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/lots');
+              },
+              child: const Text('Lots'),
             ),
           ],
         ),
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,8 @@
-import 'package:agroxpert/screens/lots.dart';
+/* import 'package:agroxpert/screens/lots.dart';
 import 'package:flutter/material.dart';
 
 import '../main.dart';
+import '../screens/estimarProduccionView.dart';
 import '../screens/final_report.dart';
 import '../screens/harvests.dart';
 
@@ -11,4 +12,38 @@ final Map<String, WidgetBuilder> routes = {
   '/lots': (BuildContext context) => const LotsScreen(),
   '/harvests': (BuildContext context) => const HarvestScreen(),
   '/final_report': (BuildContext context) => const FinalReportScreen(),
+  '/estimates_production': (BuildContext context) => const EstimatesProductionScreen(),
+
 };
+ */
+
+import 'package:flutter/material.dart';
+import '../screens/estimarProduccionView.dart';
+import '../main.dart';
+import '../screens/estimarProduccionView.dart';
+import '../screens/final_report.dart';
+import '../screens/harvests.dart';
+import '../screens/lots.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/estimates_production':
+        return MaterialPageRoute(builder: (_) => EstimatesProductionScreen());
+      case '/lots':
+        return MaterialPageRoute(builder: (_) => LotsScreen());
+      case '/harvests':
+        return MaterialPageRoute(builder: (_) => HarvestScreen());
+      case '/final_report':
+        return MaterialPageRoute(builder: (_) => FinalReportScreen());
+      // Agrega más rutas aquí si es necesario
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child: Text('La ruta ${settings.name} no se ha definido.'),
+                  ),
+                ));
+    }
+  }
+}
