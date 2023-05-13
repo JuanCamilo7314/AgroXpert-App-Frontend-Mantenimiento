9d64e6f225e55782554eceed6f91ad847ee32a4d
josamarb <sammy99marin@gmail.com>
2023-05-12 20:57:24 -0500

    Corrección de conexión de lotes

--- lib/screens/lots.dart
+++ lib/screens/lots.dart
@@ -2,8 +2,8 @@ import 'package:agroxpert/models/farm_lot_model.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/card_lots.dart';
-import '../services/estimation_api.dart';
-// import '../services/farm_lot_api.dart';
+// import '../services/estimation_api.dart';
+import '../services/farm_lot_api.dart';
 
 class LotsScreen extends StatefulWidget {
   const LotsScreen({super.key});
